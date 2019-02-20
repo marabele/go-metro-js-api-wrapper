20cf9c24b9a757038bbeabd499bd1adb0aadaa11
Luxolo Mbaleni <luxolo@platinumseed.com>
2019-02-20 10:52:40 +0200

    initial version

---
+++ README.md
@@ -0,0 +1,8 @@
+## Cape Town Public Transport Accissibility
+
+This package helps determine how accissible a location is to public transport routes.
+
+The score is from 0 to 100. 
+
+The closer to 100 a score is to 100 the better the quality of public transport is in that area. 
+
---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const axios = require('axios');
+
+module.exports = {
+    getAccissibilityScore: (a,b) => {
+        axios.get('http://proserver.gometro.co.za/api/v1/accessibility/'+a+'/'+b+'/800/')
+        .then((response)=>{
+            return response.data.accessibilityScore;
+        }).catch((error) => {
+            return 'an error occured, please make sure that the longitude and latitude is in a string format.'
+        });
+    }
+}
