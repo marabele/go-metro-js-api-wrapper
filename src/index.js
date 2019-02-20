5ccd6fe6a8a7d3a1daa0b2822628b7d1ebb65ce8
Luxolo Mbaleni <luxolo@platinumseed.com>
2019-02-20 11:38:29 +0200

    remove ninja code, use descriptive parameter variables instead

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@
 const axios = require('axios');
 
 module.exports = {
-    getAccessibilityScore: (a,b) => {
-        axios.get('http://proserver.gometro.co.za/api/v1/accessibility/'+a+'/'+b+'/800/')
+    getAccessibilityScore: (longitude,latitude) => {
+        axios.get('http://proserver.gometro.co.za/api/v1/accessibility/'+longitude+'/'+latitude+'/800/')
         .then((response)=>{
             return response.data.accessibilityScore;
         }).catch((error) => {
